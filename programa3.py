8492a3b59c241336ed893dcff35167ae44012713
Axel Rodriguez <axelrodriguezprd@uanl.edu.mx>
2020-07-09 11:24:38 -0500

    Programa 3 verson 2

--- programa3.py
+++ programa3.py
@@ -1,44 +1,37 @@
 '''
 Escribir un programa que muestre al usuario el siguiente menu
 '''
-
-switch_menu = True
-
-while switch_menu == True:
+while True:
     print('''
     Menu principal
 
-    S. suma
+    S. Suma
     R. Resta
     M. Multiplicacion
     D. Division
     A. Salir
     ''')
     opcion_usuario = input("Elige una opcion: ").upper()
+    if opcion_usuario == "S" or opcion_usuario == "R" or opcion_usuario == "M" or opcion_usuario == "D":
 
-    if opcion_usuario == "S":
         num1 = int(input("Dime el primer numero: "))
         num2 = int(input("Dime el segundo numero: "))
-        print(f"La suma de los numeros es: {num1 + num2}")
 
-    elif opcion_usuario == "R":
-        num1 = int(input("Dime el primer numero: "))
-        num2 = int(input("Dime el segundo numero: "))
-        print(f"La resta de los numeros es: {num1 - num2}")
+        if opcion_usuario == "S":
+            print(f"La suma de los numeros es: {num1 + num2}")
 
-    elif opcion_usuario == "M":
-        num1 = int(input("Dime el primer numero: "))
-        num2 = int(input("Dime el segundo numero: "))
-        print(f"La multiplicacion de los numeros es: {num1 * num2}")
+        elif opcion_usuario == "R":
+            print(f"La resta de los numeros es: {num1 - num2}")
+
+        elif opcion_usuario == "M":
+            print(f"La multiplicacion de los numeros es: {num1 * num2}")
     
-    elif opcion_usuario == "D":
-        num1 = int(input("Dime el primer numero: "))
-        num2 = int(input("Dime el segundo numero: "))
-        print(f"La division de los numeros es: {num1 / num2}")
+        elif opcion_usuario == "D":
+            print(f"La division de los numeros es: {num1 / num2}")
 
     elif opcion_usuario == "A":
-        print("Saliendo del programa...")
-        switch_menu = False
-    
-    else: 
-        print("Digite una opcion valida")
+            print("Saliendo del programa...")
+            break
+    else:
+        if opcion_usuario != "SRMDA":
+            print("Opcion invalida")
